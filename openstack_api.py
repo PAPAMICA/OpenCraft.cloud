50c4d9780a04400381b557e5248cb53d7d43dc03
PAPAMICA <mickael@papamica.com>
2022-03-01 10:51:11 +0100

    Ajout de l'API OpenStack

--- openstack_api.py
+++ openstack_api.py
@@ -9,7 +9,6 @@ import json
 import argparse
 import subprocess
 import sys
-#from flask import Flask, request, render_template, redirect
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--json", help="Output as json", action="store_true")
@@ -69,8 +68,6 @@ def cloud_connection(cloud_name):
 
 
 # Get all informations of all instances
-#@app.route("/list", methods=['GET','POST'])
-
 def get_instances_list(cloud):
     if arg_dict == 1:
         result = {}
@@ -105,7 +102,6 @@ def get_instances_list(cloud):
     return result    
 
 # Find and display information about one instance
-#@app.route("/get_instance_information")
 def get_instance_information(cloud, server_name):
     try:
         if arg_dict == 1:
@@ -143,28 +139,7 @@ def get_instance_information(cloud, server_name):
         return (f"{server_name} not found !")
 
 
-# Create Keypair
-#@app.route("/create_keypair")
-def create_keypair(cloud, keypair_name):
-    keypair = cloud.compute.find_keypair(keypair_name)
-
-    if not keypair:
-        keypair = cloud.compute.create_keypair(name=keypair_name)
-        if arg_dict == 1:
-            data = {'Private Key': keypair.private_key}
-            return data
-        elif arg_json == 1:
-            data = {'Private Key': keypair.private_key}
-            data = json.dumps(data, indent = 4)
-            return data
-        else:
-            print (keypair.private_key)
-            return keypair.private_key
-    else:
-        return keypair.private_key
-
 # List keypairs
-#@app.route("/list_keypairs")
 def list_keypairs(cloud):
     if arg_dict == 1:
         result = {}
@@ -186,7 +161,6 @@ def list_keypairs(cloud):
     return result
 
 # List networks
-#@app.route("/list_networks")
 def list_networks(cloud):
     if arg_dict == 1:
         result = {}
@@ -208,7 +182,6 @@ def list_networks(cloud):
     return result
 
 # List security groups
-#@app.route("/list_security_groups")
 def list_security_groups(cloud):
     if arg_dict == 1:
         result = {}
@@ -230,7 +203,6 @@ def list_security_groups(cloud):
     return result
 
 # List images
-#@app.route("/list_images")
 def list_images(cloud):
     if arg_dict == 1:
         result = {}
@@ -253,7 +225,6 @@ def list_images(cloud):
 
 
 # List flavors
-#@app.route("/list_flavors")
 def list_flavors(cloud):
     if arg_dict == 1:
         result = {}
@@ -274,62 +245,3 @@ def list_flavors(cloud):
 
     return result
 
-# Create instance
-#@app.route("/create_server")
-def create_instance(cloud, instance_name,instance_image, instance_flavor, instance_network, instance_keypair, instance_securitygroup):
-    debug = list_keypairs(cloud)
-    print(f'ICI 1 : {debug}', flush=True, file=sys.stdout)
-    image = cloud.compute.find_image(instance_image)
-    flavor = cloud.compute.find_flavor(instance_flavor)
-    network = cloud.network.find_network(instance_network)
-    keypair = cloud.compute.find_keypair(instance_keypair)
-    security_group = cloud.network.find_security_group(instance_securitygroup)
-
-    server = cloud.compute.create_server(
-        name=instance_name, image_id=image.id, flavor_id=flavor.id,
-        networks=[{"uuid": network.id}], key_name=keypair.name)
-
-    server = cloud.compute.wait_for_server(server)
-    server = cloud.compute.add_security_group_to_server(server, security_group)
-    get_instance_information(cloud, instance_name)
-
-
-# Start instance
-#@app.route("/start_instance")
-def start_instance(cloud, server_name):
-    try:
-        server = cloud.compute.find_server(server_name)
-        server = cloud.compute. start_server(server.id)
-        return f"[SUCCESS] {server_name} has been started !"
-    except:
-        return f"[ERROR] Can't start instance {server_name} !"
-
-# Stop instance
-#@app.route("/stop_instance")
-def stop_instance(cloud, server_name):
-    try:
-        server = cloud.compute.find_server(server_name)
-        server = cloud.compute. stop_server(server.id)
-        return f"[SUCCESS] {server_name} has been stopped !"
-    except:
-        return f"[ERROR] Can't stop instance {server_name} !"
-
-# Reboot instance
-#@app.route("/reboot_instance")
-def reboot_instance(cloud, server_name):
-    try:
-        server = cloud.compute.find_server(server_name)
-        server = cloud.compute. reboot_server(server.id, "HARD")
-        return f"[SUCCESS] {server_name} has been rebooted !"
-    except:
-        return f"[ERROR] Can't reboot instance {server_name} !"
-
-# Delete instance
-#@app.route("/delete_instance")
-def delete_instance(cloud, server_name):
-    try:
-        server = cloud.compute.find_server(server_name)
-        server = cloud.compute. delete_server(server.id)
-        return f"[SUCCESS] {server_name} has been deleted !"
-    except:
-        return f"[ERROR] Can't delete instance {server_name} !"
