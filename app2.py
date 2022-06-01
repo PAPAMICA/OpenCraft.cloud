a0039715b21082639abd3cb734190e00c38301b4
PAPAMICA <mickael@papamica.com>
2022-06-01 21:14:30 +0200

    fix container

--- app2.py
+++ app2.py
@@ -3,7 +3,7 @@
 
 import docker_api as docker
 import argparse
-from diagrams import Diagram, Cluster
+from diagrams import Diagram, Cluster, Node
 from diagrams.custom import Custom
 
 parser = argparse.ArgumentParser(description='Create a graph of your Docker project.')
@@ -30,6 +30,7 @@ edge_attr = {
 
 def create_diagram():
     with Diagram("\nDocker infrastructure", show=False, direction="TB", graph_attr=graph_attr, node_attr=tag_attr,edge_attr=edge_attr) as diag:
+        blank = Node("", shape="plaintext", height="0.0", width="0.0")
         img_network = Custom(f"{args.network}\nSubnet: {_network[args.network]['Subnet']}", "./img/internet.png")
         containers = []
         for container in _network[args.network]['Containers']:
@@ -39,7 +40,7 @@ def create_diagram():
             containers.append(img_container)
 
             
-        img_network >> containers
+        img_network >> blank >> containers
 
        
     diag
