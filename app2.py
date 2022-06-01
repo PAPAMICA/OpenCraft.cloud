cf6efce9ac279d0ced24076173a6468b72ff06fa
PAPAMICA <mickael@papamica.com>
2022-06-01 21:52:02 +0200

    fix container

--- app2.py
+++ app2.py
@@ -35,8 +35,8 @@ edge_attr = {
 def create_diagram():
     with Diagram("\nDocker infrastructure", show=False, direction="TB", graph_attr=graph_attr, node_attr=tag_attr,edge_attr=edge_attr) as diag:
         blank = Node("", shape="plaintext", height="0.0", width="0.0")
+        img_network = Custom(f"{args.network}\nSubnet: {_network[args.network]['Subnet']}", "./img/internet.png")
         with Cluster("Containers", graph_attr=cluster_attr) as containers_cluster:
-            img_network = Custom(f"{args.network}\nSubnet: {_network[args.network]['Subnet']}", "./img/internet.png")
             containers = []
             for container in _network[args.network]['Containers']:
                 img_container = Custom(
