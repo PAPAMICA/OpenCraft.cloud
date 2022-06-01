aaea543c7a7e27dc70d432e1eb51b7c2d19bc99d
PAPAMICA <mickael@papamica.com>
2022-06-01 21:59:50 +0200

    try to add graph

--- app2.py
+++ app2.py
@@ -36,7 +36,7 @@ def create_diagram():
     with Diagram("\nDocker infrastructure", show=False, direction="TB", graph_attr=graph_attr, node_attr=tag_attr,edge_attr=edge_attr) as diag:
         blank = Node("", shape="plaintext", height="0.0", width="0.0")
         img_network = Custom(f"{args.network}\nSubnet: {_network[args.network]['Subnet']}", "./img/internet.png")
-        with Cluster("Containers", graph_attr=cluster_attr) as containers_cluster:
+        with Cluster("containers", graph_attr=cluster_attr):
             containers = []
             for container in _network[args.network]['Containers']:
                 img_container = Custom(
@@ -45,7 +45,7 @@ def create_diagram():
                 containers.append(img_container)
 
             
-        img_network >> Edge(lhead=containers_cluster) >> containers
+        img_network >> Edge(lhead='cluster_containers') >> containers
 
        
     diag
